3e7d32c53b662bd71ea66aa1c5d88f96bbcc136a
Gommy111 <30302064+Gommy111@users.noreply.github.com>
2020-02-07 17:01:03 +0100

    Delete Pyramide.py

--- Pyramide.py
+++
@@ -1,9 +0,0 @@
-def print_pyramide(size):
-    for i in range(size):
-        print('*' * (i + 1))
-    for i in reversed(range(size - 1)):
-        print('*' * (i + 1))
-
-
-if __name__ == "__main__":
-    print_pyramide(int(input("Hoe groot? ")))
